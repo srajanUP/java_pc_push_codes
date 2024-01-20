e1437fc4f9e1050e77afffd35796291dad6da974
srajanUP <srajanupadhyay251@gmial.com>
2024-01-20 14:50:15 +0530

    adding files on 20/01/2022

---
+++ DSA_In_java/Sorting/Quick_Sort.java
@@ -0,0 +1,92 @@
+package DSA_In_java.Sorting;
+
+public class Quick_Sort {
+    
+    //sorting
+    static void Quicksort(int[] arr,int s, int e){
+        //base case
+        if(e<=s){
+            return;
+        }
+
+        //partition
+        int partitionIndex=partition(arr,s,e);
+
+        //sorting first half
+        Quicksort(arr, s, partitionIndex-1);
+        
+        //sorting second half
+        Quicksort(arr, partitionIndex+1, e);
+    }
+
+
+    //partition function
+     static int partition(int[] arr,int s,int e){
+        int pivot=arr[s];
+
+        //placing the pivot element on it's position
+        //determining the position of the pivot by coounting the elements lesser then pivot
+
+        int count=0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<pivot){
+                count++;
+            }
+            
+        }
+
+        int pivotindex=count;
+
+
+        //swapping the pivotindex element with pivot
+        int temp=arr[pivotindex];
+        arr[pivotindex]=pivot;
+        arr[s]=temp;
+
+
+        //checking if the elements left and right side are at right sides
+
+        int i=s;
+        int j=e;
+
+        while (i<pivotindex && j>pivotindex) {
+            //if arr[i] is at right position 
+            while (arr[i]<pivot) {
+                i++;
+            }
+            
+            //if arr[j] is at right position 
+            while (pivot<arr[j]) {
+                j--;
+                
+            }
+            
+            // swapping if the elements are at wrong position
+            if (arr[i]>pivot && arr[j]<pivot){
+                int temp1=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp1;
+
+            }
+            
+        }
+        //returning the pivot index
+        return pivotindex;
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        int arr[]={6,5,4,3,2,1,9,10};
+
+        Quicksort(arr,0,arr.length-1);
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+    
+}
---
+++ DSA_In_java/Sorting/insertionSort.java
@@ -0,0 +1,38 @@
+package DSA_In_java.Sorting;
+
+public class insertionSort {
+
+
+    public static void insertion_Sort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            int temp=arr[i];
+            int j = i-1;
+            for (; j>=0; j--) {   //shifting loop
+                if (temp>arr[j]){
+                    arr[j+1]=arr[j];
+                                                            
+                }
+                else{
+                    break;
+                }
+            }
+            arr[j+1]=temp;
+        }
+
+
+        //printing array
+        for (int i : arr) {
+            System.out.print(i+" ");
+            
+           }
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr={8,7,6,9,3,4};
+        insertion_Sort(arr);
+        
+    }
+    
+}
